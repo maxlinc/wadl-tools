8e73572d7767a30a1d2e33e3a6d813196d2c1461
Max Lincoln <max@devopsy.com>
2014-10-31 16:52:22 -0400

    Add line numbers and validateAll

--- src/main/scala/wadl-program-listing-validator.scala
+++ src/main/scala/wadl-program-listing-validator.scala
@@ -3,31 +3,74 @@ package com.rackspace.cloud.api.wadl
 import java.io.File
 import org.json4s._
 import org.json4s.jackson.JsonMethods._
-import scala.xml._
 import com.typesafe.scalalogging.slf4j.LazyLogging
+import com.rackspace.cloud.api.wadl.util.XMLWithLocationLoader
+import scala.collection.mutable.ListBuffer
+
+class WADLValidationException(msg: String) extends RuntimeException(msg) {
+  def this(msg: String, causes: List[Throwable]) = {
+    this(msg + causes.map( c => c.getMessage ).mkString("\n  "))
+  }
+}
 
 class InvalidProgramListingException(msg: String, cause: Throwable = null) extends RuntimeException(msg, cause)
 
 class WADLProgramListingValidator() extends LazyLogging {
-  def validate(xml : scala.xml.Elem) : Boolean = {
-    for (listing <- each_program_listing(xml)) {
-      try {
-        (listing \ "@language").text match {
-          case "json" | "javascript" => parse(listing.text)
-          case "xml" => XML.loadString(listing.text)
-        }
-      } catch {
-        case e: Exception => throw new InvalidProgramListingException("Invalid programlisting found (location unknown)", e)
+  def validate(listing : scala.xml.Node) : Boolean = {
+    val lineNumber = listing \ "@line"
+    val column = listing \ "@column"
+
+    try {
+      (listing \ "@language").text match {
+        case "json" | "javascript" => parse(listing.text)
+        case "xml" => XMLWithLocationLoader.loadString(listing.text)
+      }
+    } catch {
+      case cause: Exception => {
+        val msg = cause.getMessage
+        val error = new InvalidProgramListingException(s"Invalid programlisting found (line: $lineNumber, column: $column): $msg", cause)
+        logger.error(error.getMessage)
+        throw error
       }
     }
+
     true
   }
 
-  def validate(xml : File) : Boolean = {
-    validate(scala.xml.XML.loadFile(xml))
+  def validateAll(xml : scala.xml.Elem) : Boolean = {
+    val exceptions = new ListBuffer[InvalidProgramListingException]()
+
+    for (listing <- eachProgramListing(xml)) {
+      try {
+        validate(listing)
+      } catch {
+        case e: InvalidProgramListingException => {
+          exceptions += e
+        }
+      }
+    }
+
+    if (!exceptions.isEmpty) {
+      val error = new WADLValidationException("Invalid progamlistings detected: ", exceptions.toList)
+      throw error
+    }
+
+    true
   }
 
-  def each_program_listing(xml : scala.xml.Elem) = {
+  def validateAll(string : String) : Boolean = {
+    validateAll(XMLWithLocationLoader.loadString(string))
+  }
+
+  def validateAll(file : File) : Boolean = {
+    validateAll(XMLWithLocationLoader.loadFile(file))
+  }
+
+  def validate(string : String) : Boolean = {
+    validate(XMLWithLocationLoader.loadString(string))
+  }
+
+  def eachProgramListing(xml : scala.xml.Elem) = {
     for {
       node <- xml \\ "programlisting"
     } yield {node}
--- src/test/scala/test-validate.scala
+++ src/test/scala/test-validate.scala
@@ -20,6 +20,7 @@ import java.io.ByteArrayOutputStream
 import javax.xml.transform.stream.StreamSource
 import javax.xml.transform.stream.StreamResult
 import com.rackspace.cloud.api.wadl.InvalidProgramListingException
+import com.rackspace.cloud.api.wadl.WADLValidationException
 
 @RunWith(classOf[JUnitRunner])
 class BadProgramListingSpec extends BaseProgramListingSpec with LazyLogging {
@@ -31,144 +32,93 @@ class BadProgramListingSpec extends BaseProgramListingSpec with LazyLogging {
   register ("wadl","http://wadl.dev.java.net/2009/02")
 
   feature ("The WADLProgramListingValidator should validate program listing syntax for the specified language.") {
-    scenario ("A WADL with an invalid json program listing should not be validated") {
-     Given("a WADL with an invalid json program listing")
-     val inWADL =
-        <application xmlns="http://wadl.dev.java.net/2009/02"
-                     xmlns:xsd="http://www.w3.org/2001/XMLSchema">
-          <resources base="https://test.api.openstack.com">
-            <resource path="a/b">
-              <resource path="c">
-                <method name="POST" rax:id="createNotification">
-                  <request>
-                    <representation mediaType="application/xml" xml:lang="EN" element="csapi">
-                      <wadl:doc xmlns="http://docbook.org/ns/docbook" title="Create webhook notification: XML request">
-                        <xsdxt:code>
-                          <programlisting language="json">
-                          {raw"""{"label": "my webhook #1", "type": "webhook""details":{"url": "https://systems.example.org/alert"} } """}
-                          </programlisting>
-                        </xsdxt:code>
-                      </wadl:doc>
-                    </representation>
-                  </request>
-                </method>
-              </resource>
-            </resource>
-          </resources>
-        </application>
+    scenario ("an invalid json program listing should not be validated") {
+     Given("an invalid json program listing")
+     val listing =
+        <programlisting language="json">
+        {raw"""{"label": "my webhook #1", "type": "webhook""details":{"url": "https://systems.example.org/alert"} } """}
+        </programlisting>
       When("the WADL is validated")
       val thrown = intercept[InvalidProgramListingException] {
-        val result = wadl.validate(inWADL)
+        val result = wadl.validate(listing.toString)
       }
       Then("The result should be foo")
-      assert(thrown.getMessage().contains("Invalid programlisting found"))
+      assert(thrown.getMessage().contains("Invalid programlisting found (line: 3, column: 26)"))
     }
 
-    scenario ("A WADL with an valid json program listing should be validated") {
-     Given("a WADL with an valid json program listing")
-     val inWADL =
-        <application xmlns="http://wadl.dev.java.net/2009/02"
-                     xmlns:xsd="http://www.w3.org/2001/XMLSchema">
-          <resources base="https://test.api.openstack.com">
-            <resource path="a/b">
-              <resource path="c">
-                <method name="POST" rax:id="createNotification">
-                  <request>
-                    <representation mediaType="application/xml" xml:lang="EN" element="csapi">
-                      <wadl:doc xmlns="http://docbook.org/ns/docbook" title="Create webhook notification: XML request">
-                        <xsdxt:code>
-                          <programlisting language="json">
-                          {raw"""{"label": "my webhook #1", "type": "webhook", "details":{"url": "https://systems.example.org/alert"} } """}
-                          </programlisting>
-                        </xsdxt:code>
-                      </wadl:doc>
-                    </representation>
-                  </request>
-                </method>
-              </resource>
-            </resource>
-          </resources>
-        </application>
+    scenario ("a valid json program listing should be validated") {
+     Given("a valid json program listing")
+     val listing =
+        <programlisting language="json">
+        {raw"""{"label": "my webhook #1", "type": "webhook", "details":{"url": "https://systems.example.org/alert"} } """}
+        </programlisting>
       When("the WADL is validated")
-      val result = wadl.validate(inWADL)
+      val result = wadl.validate(listing.toString)
       Then("The result should be true")
       assert(result == true)
     }
 
-    scenario ("A WADL with an invalid xml program listing should not be validated") {
-     Given("a WADL with an invalid json program listing")
-     val inWADL =
-        <application xmlns="http://wadl.dev.java.net/2009/02"
-                     xmlns:xsd="http://www.w3.org/2001/XMLSchema">
-          <resources base="https://test.api.openstack.com">
-            <resource path="a/b">
-              <resource path="c">
-                <method name="POST" rax:id="createNotification">
-                  <request>
-                    <representation mediaType="application/xml" xml:lang="EN" element="csapi">
-                      <wadl:doc xmlns="http://docbook.org/ns/docbook" title="Create webhook notification: XML request">
-                        <xsdxt:code>
-                          <programlisting language="xml">&lt;?xml version="1.0" encoding="utf-8"?&gt;
-                          &lt;notification&gt;
-                            &lt;label&gt;my webhook #1&lt;/label&gt;
-                            &lt;type&gt;webhook&lt;/type&gt;
-                            &lt;details&gt;
-                              &lt;url&gt;https://systems.example.org/alert&lt;/url&gt;
-                            &lt;details&gt;
-                          &lt;/notification&gt;
-                          </programlisting>
-                        </xsdxt:code>
-                      </wadl:doc>
-                    </representation>
-                  </request>
-                </method>
-              </resource>
-            </resource>
-          </resources>
-        </application>
+    scenario ("an invalid xml program listing should not be validated") {
+     Given("an invalid json program listing")
+     val listing =
+        <programlisting language="xml">&lt;?xml version="1.0" encoding="utf-8"?&gt;
+        &lt;notification&gt;
+          &lt;label&gt;my webhook #1&lt;/label&gt;
+          &lt;type&gt;webhook&lt;/type&gt;
+          &lt;details&gt;
+            &lt;url&gt;https://systems.example.org/alert&lt;/url&gt;
+          &lt;details&gt;
+        &lt;/notification&gt;
+        </programlisting>
       When("the WADL is validated")
       val thrown = intercept[InvalidProgramListingException] {
-        val result = wadl.validate(inWADL)
+        val result = wadl.validate(listing.toString)
       }
       Then("The result should be foo")
-      assert(thrown.getMessage().contains("Invalid programlisting found"))
+      assert(thrown.getMessage().contains("Invalid programlisting found (line: 9, column: 26):"))
     }
 
-    scenario ("A WADL with an valid xml program listing should be validated") {
-     Given("a WADL with an valid xml program listing")
-     val inWADL =
-        <application xmlns="http://wadl.dev.java.net/2009/02"
-                     xmlns:xsd="http://www.w3.org/2001/XMLSchema">
-          <resources base="https://test.api.openstack.com">
-            <resource path="a/b">
-              <resource path="c">
-                <method name="POST" rax:id="createNotification">
-                  <request>
-                    <representation mediaType="application/xml" xml:lang="EN" element="csapi">
-                      <wadl:doc xmlns="http://docbook.org/ns/docbook" title="Create webhook notification: XML request">
-                        <xsdxt:code>
-                          <programlisting language="xml">&lt;?xml version="1.0" encoding="utf-8"?&gt;
-                          &lt;notification&gt;
-                            &lt;label&gt;my webhook #1&lt;/label&gt;
-                            &lt;type&gt;webhook&lt;/type&gt;
-                            &lt;details&gt;
-                              &lt;url&gt;https://systems.example.org/alert&lt;/url&gt;
-                            &lt;/details&gt;
-                          &lt;/notification&gt;
-                          </programlisting>
-                        </xsdxt:code>
-                      </wadl:doc>
-                    </representation>
-                  </request>
-                </method>
-              </resource>
-            </resource>
-          </resources>
-        </application>
+    scenario ("a valid xml program listing should be validated") {
+     Given("a valid xml program listing")
+     val listing =
+        <programlisting language="xml">&lt;?xml version="1.0" encoding="utf-8"?&gt;
+        &lt;notification&gt;
+          &lt;label&gt;my webhook #1&lt;/label&gt;
+          &lt;type&gt;webhook&lt;/type&gt;
+          &lt;details&gt;
+            &lt;url&gt;https://systems.example.org/alert&lt;/url&gt;
+          &lt;/details&gt;
+        &lt;/notification&gt;
+        </programlisting>
       When("the WADL is validated")
-      val result = wadl.validate(inWADL)
+      val result = wadl.validate(listing.toString)
       Then("The result should be true")
       assert(result == true)
+    }
+  }
+
+  feature ("The WADLProgramListingValidator validateAll should report about all exceptions.") {
+    scenario ("a WADL file with no invalid programlistings") {
+      Given("a WADL file with all valid programlisting")
+      val inWADL = new File("src/test/resources/fixtures/valid_listings.wadl")
+
+      When("the WADL is fully validated")
+      val result = wadl.validateAll(inWADL)
+      Then("The result should be true")
+      assert(result == true)
+    }
+
+    scenario ("a WADL file with some invalid programlistings") {
+      Given("a WADL file with some invalid programlistings")
+      val inWADL = new File("src/test/resources/fixtures/invalid_listings.wadl")
+      When("the WADL is fully validated")
+      val thrown = intercept[WADLValidationException] {
+        val result = wadl.validateAll(inWADL)
+      }
+      Then("The result should be true")
+      val errorMsg = thrown.getMessage()
+      assert(errorMsg.contains("Invalid progamlistings detected"))
+      assert(errorMsg.contains("Invalid programlisting found (line:"))
     }
   }
 }
